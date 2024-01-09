05c2dbde63b23d75538fed4a2892244fb82187a6
tracer-ysyx <tracer@ysyx.org>
2024-01-09 23:30:36 +0800

    > compile NEMU ysyx_22040000 niehuaihao Linux nhh-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:30:36 up 12 min,  1 user,  load average: 0.00, 0.01, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -182,10 +182,161 @@ static bool make_token(char *e)
   return true;
 }
 
-// static bool check_parentheses(int p, int q, bool *success)
-// {
+static bool check_parentheses(int p, int q)
+{
+  if (tokens[p].type != '(' || tokens[q].type != ')')
+  {
+    return false;
+  }
 
-// }
+  int l = p;
+  int r = q;
+  while (l < r)
+  {
+    if (tokens[l].type == '(')
+    {
+      if (tokens[l].type == '(')
+      {
+        l++;
+        r--;
+        continue;
+      }
+      else
+      {
+        r--;
+      }
+    }
+    else if (tokens[l].type == ')')
+    {
+      return false;
+    }
+    else
+    {
+      l++;
+    }
+  }
+  return true;
+}
+
+int max(int a, int b)
+{
+  if (a > b)
+    return a;
+  else
+    return b;
+}
+
+uint32_t eval(int p, int q)
+{
+  if (p > q)
+  {
+    /* Bad expression */
+    assert(0);
+    return -1;
+  }
+  else if (p == q)
+  {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    return atoi(tokens[p].str); // 将字符串转换成整数
+  }
+  else if (check_parentheses(p, q) == true)
+  {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1);
+  }
+  else
+  {
+    /* We should do more things here. */
+    int op = -1; // op是主运算符
+    bool flag = false;
+    for (int i = p; i <= q; i++)
+    {
+      if (tokens[i].type == '(')
+      {
+        while (tokens[i].type != ')')
+        {
+          i++;
+        }
+      }
+
+      if (!flag && tokens[i].type == TK_OR)
+      {
+        flag = true;
+        op = max(op, i);
+      }
+
+      if (!flag && tokens[i].type == TK_AND)
+      {
+        flag = true;
+        op = max(op, i);
+      }
+
+      if (!flag && tokens[i].type == TK_NOTEQ)
+      {
+        flag = true;
+        op = max(op, i);
+      }
+
+      if (!flag && tokens[i].type == TK_EQ)
+      {
+        flag = true;
+        op = max(op, i);
+      }
+
+      if (!flag && (tokens[i].type == '+' || tokens[i].type == '-'))
+      {
+        flag = true;
+        op = max(op, i);
+      }
+
+      if (!flag && (tokens[i].type == '*' || tokens[i].type == '/'))
+      {
+        op = max(op, i);
+      }
+    }
+    printf("主运算符位置是：%d.\n", op);
+    int op_type = tokens[op].type; // 主运算符属性
+    printf("主运算符是：%d.\n", op_type);
+
+    // 递归处理剩余的部分
+    uint32_t val1 = eval(p, op - 1);
+    uint32_t val2 = eval(op + 1, q);
+
+    switch (op_type)
+    {
+    case '+':
+      return val1 + val2;
+    case '-':
+      return val1 - val2;
+    case '*':
+      return val1 * val2;
+    case '/':
+      if (val2 == 0)
+      {
+        printf("val2被除数不可以是0");
+        break;
+      }
+      return val1 / val2;
+    case TK_EQ:
+      return val1 == val2;
+    case TK_NOTEQ:
+      return val1 != val2;
+    case TK_OR:
+      return val1 || val2;
+    case TK_AND:
+      return val1 && val2;
+    default:
+      printf("没有主运算符.");
+      assert(0);
+    }
+  }
+  return 0;
+}
 
 word_t expr(char *e, bool *success)
 {
@@ -195,7 +346,5 @@ word_t expr(char *e, bool *success)
     return 0;
   }
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  return eval(0, *e);
 }

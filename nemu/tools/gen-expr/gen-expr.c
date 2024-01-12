a4146ae8822977f17e67b4394b6e8caa3ba31e0b
tracer-ysyx <tracer@ysyx.org>
2024-01-12 23:42:11 +0800

    > compile NEMU ysyx_22040000 niehuaihao Linux nhh-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:11 up  1:22,  1 user,  load average: 0.71, 0.59, 0.27

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -12,22 +12,6 @@
  *
  * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
-
-/***************************************************************************************
- * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
- *
- * NEMU is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *          http://license.coscl.org.cn/MulanPSL2
- *
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- *
- * See the Mulan PSL v2 for more details.
- ***************************************************************************************/
-
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,93 +29,75 @@ static char *code_format =
     "  printf(\"%%u\", result); "
     "  return 0; "
     "}";
-static int len = 0;
 
-// 随机生成数字
-static void gen_num(int l)
+static void gen_num()
 {
-  // 第一位不能是0
-  buf[len++] = '0' + rand() % 9 + 1; // 1~9之间
-  --l;
-  for (int i = 0; i < l; i++)
-  {
-    buf[len++] = '0' + rand() % 10;
-  }
-  buf[len++] = 'u';
-  buf[len++] = 'l';
-  buf[len++] = 'l';
+  sprintf(buf + strlen(buf), "%uu", rand() % 100);
+  buf[strlen(buf)] = '\0';
 }
 
-// 随机生成空格
-static void rand_whitespace()
+static void gen(char a)
 {
-  int num = rand() % 10 + 1;
-  for (int i = 0; i < num; i++)
-  {
-    if (rand() % 7 == 2)
-    {
-      buf[len++] = ' ';
-    }
-  }
+  sprintf(buf + strlen(buf), "%c", a);
+  buf[strlen(buf)] = '\0';
 }
 
-// 随机生成表达式
-static void gen_rand_expr(int dep)
+static void gen_rand_op()
 {
-  if (dep = 0)
-  {
-    len = 0;
-  }
-  if (dep > 50)
-  {
-    rand_whitespace();
-    gen_num(rand() % 10 + 1);
-    rand_whitespace();
-    return;
-  }
-  switch (rand() % 3)
+  char op = 0;
+  switch (rand() % 4)
   {
   case 0:
-    rand_whitespace();
-    gen_num(rand() % 16 + 1);
-    rand_whitespace();
+    op = '+';
     break;
   case 1:
-    rand_whitespace();
-    buf[len++] = '(';
-    rand_whitespace();
-    gen_rand_expr(dep + 1);
-    rand_whitespace();
-    buf[len++] = ')';
-    rand_whitespace();
+    op = '-';
     break;
   case 2:
-    rand_whitespace();
-    gen_rand_expr(dep + 1);
-    rand_whitespace();
-    switch (rand() % 4)
-    {
-    case 0:
-      buf[len++] = '+';
-      break;
-    case 1:
-      buf[len++] = '-';
-      break;
-    case 2:
-      buf[len++] = '*';
-      break;
-    case 3:
-      buf[len++] = '/';
-      break;
-    }
-    rand_whitespace();
-    gen_rand_expr(dep + 1);
-    rand_whitespace();
+    op = '*';
+    break;
+  case 3:
+    op = '/';
     break;
   }
-  if (dep == 0)
+  gen(op);
+}
+
+static inline void gen_rand_expr()
+{
+  if (strlen(buf) < 100)
   {
-    buf[len++] = '\0';
+    switch (rand() % 3)
+    {
+    case 0:
+      gen_num();
+      break;
+    case 1:
+      gen('(');
+      gen_rand_expr();
+      gen(')');
+      break;
+    default:
+      gen_rand_expr();
+      gen_rand_op();
+      gen_rand_expr();
+      break;
+    }
+  }
+  else
+  {
+    gen_num();
+  }
+  buf[strlen(buf)] = '\0';
+}
+
+void remove_u(char *p)
+{
+  char *q = p;
+  while ((q = strchr(q, 'u')) != NULL)
+  {
+    strcpy(code_buf, q + 1);
+    strcpy(q, code_buf);
   }
 }
 
@@ -147,6 +113,7 @@ int main(int argc, char *argv[])
   int i;
   for (i = 0; i < loop; i++)
   {
+    memset(buf, 0, sizeof(buf));
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
@@ -156,24 +123,24 @@ int main(int argc, char *argv[])
     fputs(code_buf, fp);
     fclose(fp);
 
-    unsigned long long result;
-    ret = fscanf(fp, "%llu", &result);
-    pclose(fp);
-
-    if (ret != 1)
-    {
-      --i;
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    if (ret != 0)
       continue;
-    }
-    for (int i = 0; buf[i] != '\0'; ++i)
-    {
-      if (buf[i] == 'u' || buf[i] == 'l')
-      {
-        buf[i] = ' ';
-      }
-    }
-    printf("%llu %s\n", result, buf);
-    fflush(stdout);
+
+    fp = popen("/tmp/.expr", "r");
+    assert(fp != NULL);
+
+    int result;
+    ret = fscanf(fp, "%d", &result);
+    if (ret != 1)
+      continue;
+    ret = pclose(fp);
+    if (ret != 0)
+      continue;
+
+    remove_u(buf);
+
+    printf("%u %s\n", result, buf);
   }
   return 0;
 }

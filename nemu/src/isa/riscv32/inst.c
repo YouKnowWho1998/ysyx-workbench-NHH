d64cdfb711cb0cd260ccbdef3beaa560a584ddca
tracer-ysyx <tracer@ysyx.org>
2024-05-04 16:39:47 +0800

    > compile NEMU ysyx_22040000 niehuaihao Linux nhh-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:39:47 up  1:27,  1 user,  load average: 0.20, 0.08, 0.03

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -1,17 +1,26 @@
+/*
+ * @Author       : 中北大学-聂怀昊
+ * @Date         : 2024-03-08 17:19:42
+ * @LastEditTime : 2024-05-04 16:39:28
+ * @FilePath     : \ysyx\ysyx-workbench\nemu\src\isa\riscv32\inst.c
+ * @Description  :
+ *
+ * Copyright (c) 2024 by 873040830@qq.com, All Rights Reserved.
+ */
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include "local-include/reg.h"
 #include <cpu/cpu.h>
@@ -22,47 +31,91 @@
 #define Mr vaddr_read
 #define Mw vaddr_write
 
-enum {
-  TYPE_I, TYPE_U, TYPE_S,
+enum
+{
+  TYPE_I,
+  TYPE_U,
+  TYPE_S,
   TYPE_N, // none
 };
 
-#define src1R() do { *src1 = R(rs1); } while (0)
-#define src2R() do { *src2 = R(rs2); } while (0)
-#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
-#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define src1R()     \
+  do                \
+  {                 \
+    *src1 = R(rs1); \
+  } while (0)
+#define src2R()     \
+  do                \
+  {                 \
+    *src2 = R(rs2); \
+  } while (0)
+#define immI()                        \
+  do                                  \
+  {                                   \
+    *imm = SEXT(BITS(i, 31, 20), 12); \
+  } while (0)
+#define immU()                              \
+  do                                        \
+  {                                         \
+    *imm = SEXT(BITS(i, 31, 12), 20) << 12; \
+  } while (0)
+#define immS()                                               \
+  do                                                         \
+  {                                                          \
+    *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); \
+  } while (0)
 
-static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
+static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type)
+{
   uint32_t i = s->isa.inst.val;
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
-  *rd     = BITS(i, 11, 7);
-  switch (type) {
-    case TYPE_I: src1R();          immI(); break;
-    case TYPE_U:                   immU(); break;
-    case TYPE_S: src1R(); src2R(); immS(); break;
+  *rd = BITS(i, 11, 7);
+  switch (type)
+  {
+  case TYPE_I:
+    src1R();
+    immI();
+    break;
+  case TYPE_U:
+    immU();
+    break;
+  case TYPE_S:
+    src1R();
+    src2R();
+    immS();
+    break;
   }
 }
 
-static int decode_exec(Decode *s) {
+static int decode_exec(Decode *s)
+{
   int rd = 0;
   word_t src1 = 0, src2 = 0, imm = 0;
   s->dnpc = s->snpc;
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
-#define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
-  decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
-  __VA_ARGS__ ; \
-}
+#define INSTPAT_MATCH(s, name, type, ... /* execute body */)         \
+  {                                                                  \
+    decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
+    __VA_ARGS__;                                                     \
+  }
 
   INSTPAT_START();
-  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
-  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
-  INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc, U, R(rd) = s->pc + imm);
+  INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu, I, R(rd) = Mr(src1 + imm, 1));
+  INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb, S, Mw(src1 + imm, 1, src2));
 
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
+
+  // lui指令
+  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui, U, R(rd) = imm << 12);
+  // addi指令
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I, R(rd) = src1 + imm);
+  // jal指令
+  // INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, R(rd) = s->pc + 4, s->pc += imm);
+
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
@@ -70,7 +123,8 @@ static int decode_exec(Decode *s) {
   return 0;
 }
 
-int isa_exec_once(Decode *s) {
+int isa_exec_once(Decode *s)
+{
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
   return decode_exec(s);
 }

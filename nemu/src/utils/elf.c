f4cbd050e74298a34cb68eddb8f1414b78000d33
tracer-ysyx <tracer@ysyx.org>
2024-06-14 12:18:14 +0800

    > compile NEMU ysyx_22040000 niehuaihao Linux nhh-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:18:13 up  2:18,  1 user,  load average: 0.09, 0.02, 0.01

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -1,7 +1,7 @@
 /*
  * @Author       : 中北大学-聂怀昊
  * @Date         : 2024-06-11 13:03:47
- * @LastEditTime : 2024-06-14 12:17:05
+ * @LastEditTime : 2024-06-14 12:18:06
  * @FilePath     : \ysyx\ysyx-workbench\nemu\src\utils\elf.c
  * @Description  : 解析elf文件 存入符号表中
  *
@@ -28,7 +28,7 @@ size_t symbol_tables_size; // 全局变量
 
 void parse_elf(const char *elf_file)
 {
-    printf("%s\n", elf_file);
+    printf("elf=%s\n", elf_file);
     if (elf_file == NULL)
     {
         return;

384317a317fc8abac0dc84bd19189ef88665aaee
tracer-ysyx <tracer@ysyx.org>
2024-06-23 23:58:57 +0800

    > sim RTL ysyx_22040000 niehuaihao Linux nhh-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:58:57 up 6 min,  1 user,  load average: 0.01, 0.07, 0.04

--- npc/csrc/utils/log.cpp
+++ npc/csrc/utils/log.cpp
@@ -1,11 +1,14 @@
 #include <common.h>
 #include <utils.h>
+#include <debug.h>
 
 FILE *log_fp = NULL;
 
-void init_log(const char *log_file) {
+void init_log(const char *log_file)
+{
   log_fp = stdout;
-  if (log_file != NULL) {
+  if (log_file != NULL)
+  {
     FILE *fp = fopen(log_file, "w");
     Assert(fp, "Can not open '%s'", log_file);
     log_fp = fp;

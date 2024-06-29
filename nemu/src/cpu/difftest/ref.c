b618a343c0ac2bb5a1dfd204235bed24d92116f8
ysyx-2023060191-NHH <nie@ysyx.com>
2024-06-29 23:29:58 +0800

    补齐ref.c中的difftest API函数，生成共享库SO文件

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -1,40 +1,69 @@
+
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 #include <cpu/cpu.h>
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
+{
+  if (direction == DIFFTEST_TO_REF)
+  {
+    memcpy(guest_to_host(addr), buf, n);
+  }
+  else
+  {
+    assert(0);
+  }
+}
+
+__EXPORT void difftest_regcpy(void *dut, bool direction)
+{
+  CPU_state *diff_context = (CPU_state *)dut;
+  if (direction == DIFFTEST_TO_REF)
+  {
+    for (int i = 0; i < 32; i++)
+    {
+      cpu.gpr[i] = diff_context->gpr[i];
+    }
+    cpu.pc = diff_context->pc;
+  }
+  else
+  {
+    for (int i = 0; i < 32; i++)
+    {
+      diff_context->gpr[i] = cpu.gpr[i];
+    }
+    diff_context->pc = cpu.pc;
+  }
+}
+
+__EXPORT void difftest_exec(uint64_t n)
+{
+  cpu_exec(n);
+}
+
+__EXPORT void difftest_raise_intr(word_t NO)
+{
   assert(0);
 }
 
-__EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
-}
-
-__EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
-}
-
-__EXPORT void difftest_raise_intr(word_t NO) {
-  assert(0);
-}
-
-__EXPORT void difftest_init(int port) {
+__EXPORT void difftest_init(int port)
+{
   void init_mem();
   init_mem();
   /* Perform ISA dependent initialization. */

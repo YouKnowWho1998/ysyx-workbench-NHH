fa8655fa0c4afd57e24586659f6b3ac2aed0486a
Niehuaihao-ysyx-23060191 <nhh@ysyx.com>
2024-07-16 23:13:12 +0800

    实现clock并通过tests

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint32_t h = inl(RTC_ADDR + 4);
+  uint32_t l = inl(RTC_ADDR);
+  uptime->us = (uint64_t)l + ((uint64_t)h << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {

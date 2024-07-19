27e656646a7457409bbc094a0e3223e32bbdd838
Niehuaihao-ysyx-23060191 <nhh@ysyx.com>
2024-07-19 15:24:33 +0800

    通过测试 完成PA2.3

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,15 +7,29 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  uint32_t wh_data = inl(VGACTL_ADDR);
+  uint32_t h = wh_data & 0xffff;
+  uint32_t w = wh_data >> 16;
+  *cfg = (AM_GPU_CONFIG_T){
+      .present = true, .has_accel = false, .width = w, .height = h, .vmemsz = 0};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0))
+    return;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y + h; i++)
+  {
+    for (int j = x; j < x + w; j++)
+    {
+      fb[screen_w * i + j] = pixels[w * (i - y) + (j - x)];
+    }
+  }
+  if (ctl->sync)
+  {
     outl(SYNC_ADDR, 1);
   }
 }
--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -53,6 +53,7 @@ void init_map() {
 }
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
+  IFDEF(CONFIG_DTRACE, Log("read device %s : address in  = " FMT_PADDR ", len = %d\n", map->name, addr, len));
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
@@ -62,6 +63,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
+  IFDEF(CONFIG_DTRACE, Log("write device %s : address in = " FMT_PADDR ", len = %d\n", addr, len));
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
